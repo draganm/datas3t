6a797957bddfcb5940ef61cbe6aa0f9fe7af897f
Dragan Milic <dragan@netice9.com>
2025-03-11 22:43:02 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Datas3t";
+  inputs = {
+
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.11"; };
+
+    systems.url = "github:nix-systems/default";
+
+  };
+
+  outputs = { self, nixpkgs, systems, ... }@inputs:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (system:
+          f (import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+          }));
+
+    in {
+      devShells = eachSystem (pkgs:{
+          default = pkgs.mkShell {
+            shellHook = ''
+              # Set here the env vars you want to be available in the shell
+            '';
+            hardeningDisable = [ "all" ];
+
+            packages = with pkgs; [
+              go
+              go-tools # staticccheck
+              gopls # lsp
+              gotools # goimports, ...
+              shellcheck
+              sqlc
+              sqlite
+              overmind
+            ];
+          };
+        });
+    };
+}
+
